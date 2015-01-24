bd429189f431d79a25faf5572542075e6db6858e
Benjamin Lefaudeux <benjamin.lefaudeux@gmail.com>
2015-01-24 18:25:13 +0100

    Adding a Sony QX api - from Tsar@Github

--- unit_tests/test_star.py
+++ unit_tests/test_star.py
@@ -17,19 +17,22 @@ star_list.setData('../data/hygxyz.csv')
 
 # User info
 #Direction of observation (azm,alt) in radiant
-azm = 3*pi/2 # 0: North, pi: South
-alt = pi/6 # 0: horizon, pi/2: zenith
+azm = 3*pi/2  # 0: North, pi: South
+alt = pi/6  # 0: horizon, pi/2: zenith
+
 #Position (lon,lat) in radiant
-lon = (12+34./60)*pi/180#(2+20./60)*pi/180 # >0 for East and <0 for West ???
-lat = (55+40./60)*pi/180 # >0 for North and <0 for South
+lon = (12+34./60)*pi/180  # (2+20./60)*pi/180 # >0 for East and <0 for West ???
+lat = (55+40./60)*pi/180  # >0 for North and <0 for South
+
 #Date (day,hour)
 year = 2014
-day = 154 #num of the day 1-366
+day = 154  # num of the day 1-366
 hour = 20
+
 #Group info
 #Normalize by reference date 2000 03 21 at 00 UT
-numDay = day + (year-2000)*365+(year-2000)/4 #bissextile years
-info=[azm, alt, lon, lat, numDay, hour]
+numDay = day + (year-2000)*365+(year-2000)/4  # bissextile years
+info = [azm, alt, lon, lat, numDay, hour]
 
 # Show sky
 star_list.sky(3, 5, info)
