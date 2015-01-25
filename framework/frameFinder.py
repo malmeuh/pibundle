e3370a77e7a0c42a1abd932124d55f6371545231
Benjamin Lefaudeux <benjamin.lefaudeux@gmail.com>
2015-01-25 17:29:49 +0100

    Find match in a landscape

--- framework/frameFinder.py
+++ framework/frameFinder.py
@@ -9,6 +9,7 @@ Find a picture position compared to a given landscape
 """
 
 import cv2
+import numpy as np
 
 
 class FrameFinder:
@@ -30,20 +31,31 @@ class FrameFinder:
         #  Stupid first try : go through all of the pictures and compute a first ressemblance score
         best_match_index = 0
         best_match_score = 0
+        best_correlation_map = 0
+
         index = 0
 
         for pict in self._ref_pictures:
-            score = self.compute_rough_score(pict, new_pict)
+            correlation_map = self.compute_rough_score(pict, new_pict)
+            score = np.max(correlation_map)
+
             if score > best_match_score:
                 best_match_score = score
                 best_match_index = index
+                best_correlation_map = correlation_map
 
             index += 1
 
         print "Found a match in picture {}".format(best_match_index)
 
-        return best_match_index
+        best_line = best_correlation_map.argmax(axis=0)
+        best_collumn = best_line.argmax(axis=0)
+        best_line = np.max(best_line)
+        coord = [best_line, best_collumn]
+
+        return best_match_index, coord
 
     @staticmethod
-    def compute_rough_score( pict1, pict2):
+    def compute_rough_score(pict1, pict2):
+        # pict2 is tested as beeing part of pict1 !
         return cv2.matchTemplate(pict1, pict2, method=cv2.TM_CCORR)
--- unit_tests/test_pictureFinder.py
+++ unit_tests/test_pictureFinder.py
@@ -8,8 +8,10 @@ import frameGrabber
 import os
 import time
 
-ref_pict_path = os.path.abspath("ref_picts")
-test_pict_path = os.path.abspath("test_picts")
+##############################################
+# Test a match over translated pictures
+ref_pict_path = os.path.abspath("ref_picts/padding")
+test_pict_path = os.path.abspath("test_picts/padding")
 
 # Find and load all the reference pictures
 ref_frame_source = frameGrabber.PictsFile(ref_pict_path)
@@ -26,5 +28,29 @@ for pict in test_picts:
     time_start = time.time()
     frame_finder.find_pict_pose(pict)
     time_stop = time.time()
+    print "Took {:.2f} seconds to find\n".format(time_stop - time_start)
+
+##############################################
+# Find a match in a landscape
+ref_pict_path = os.path.abspath("ref_picts/landscape")
+test_pict_path = os.path.abspath("test_picts/landscape")
+
+# Find and load all the reference pictures
+ref_frame_source = frameGrabber.PictsFile(ref_pict_path)
+ref_picts = ref_frame_source.get_pict_list()
+
+test_frame_source = frameGrabber.PictsFile(test_pict_path)
+test_picts = test_frame_source.get_pict_list()
+
+# Test the finder
+frame_finder = frameFinder.FrameFinder()
+frame_finder.declare_ref_frames(ref_picts)
+
+for pict in test_picts:
+    time_start = time.time()
+    _, coordinates = frame_finder.find_pict_pose(pict)
+    time_stop = time.time()
+
+    print "Match found, coordinates {}".format(coordinates)
     print "Took {:.2f} seconds to find".format(time_stop - time_start)
 
